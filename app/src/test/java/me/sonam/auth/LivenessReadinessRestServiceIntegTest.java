657cbdafc1844e61b0b56dc767be0836d1b338d6
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-09-26 14:18:32 -0600

    update

---
+++ app/src/test/java/me/sonam/auth/LivenessReadinessRestServiceIntegTest.java
@@ -0,0 +1,38 @@
+package me.sonam.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Test the liveness and readiness endpoints
+ */
+@AutoConfigureWebTestClient
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class})
+public class LivenessReadinessRestServiceIntegTest {
+  private static final Logger LOG = LoggerFactory.getLogger(LivenessReadinessRestServiceIntegTest.class);
+
+  @Autowired
+  private WebTestClient client;
+
+  @Test
+  public void readiness() {
+    LOG.info("check readiness endpoint");
+    client.get().uri("/token-mediator/api/health/readiness")
+            .exchange().expectStatus().isOk();
+  }
+
+  @Test
+  public void liveness() {
+    LOG.info("check liveness endpoint");
+    client.get().uri("/token-mediator/api/health/liveness")
+            .exchange().expectStatus().isOk();
+  }
+}
